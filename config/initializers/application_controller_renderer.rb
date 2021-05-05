c0a246adaad042b2d22e8ef49f212c41ef15b4d7
Fukui-S <tibita13579@yahoo.co.jp>
2021-05-05 05:51:46 +0000

    FirstCommit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '6dcdc4c7547dd11177ae04241410047c7ebf8d06b3be244d8b2dcbdfaa90559e340716fb4c15c48a3d94add51ed6d17347a2f4ef848218e926f5d6ec3c0ec51b'
